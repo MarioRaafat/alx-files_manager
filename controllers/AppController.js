d5b82dfb0c1c4ff5e1b4c6b8063224d6dec40a6b
Mario Raafat <mariorafat10@gmail.com>
2024-10-16 21:00:42 +0300

    make it promise

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,10 +2,10 @@ import redisClient from '../utils/redis.js';
 import dbClient from '../utils/db.js';
 
 export const getStats = (req, res) => {
-    res.status(200).json({
-        users: dbClient.nbUsers(),
-        files: dbClient.nbFiles(),
-    });
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+        .then(([usersCount, filesCount]) => {
+            res.status(200).json({ users: usersCount, files: filesCount });
+        });
 }
 
 export const getStatus = (req, res) => {
